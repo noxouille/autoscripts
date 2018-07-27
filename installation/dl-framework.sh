835f9d8db49e6ab3f82dd5f380c87b7e915f3fda
Nox <fp9821@gmail.com>
2018-07-27 10:02:55 +0900

    add dl framework basic script

---
+++ installation/dl-framework.sh
@@ -0,0 +1,9 @@
+VER=18.07
+
+docker pull nvcr.io/nvidia/tensorflow:$VER-py3
+docker pull nvcr.io/nvidia/mxnet:$VER-py3
+docker pull nvcr.io/nvidia/caffe:$VER-py2
+docker pull nvcr.io/nvidia/caffe2:$VER-py3
+docker pull nvcr.io/nvidia/cntk:$VER-py3
+docker pull nvcr.io/nvidia/pytorch:$VER-py3
+docker pull nvcr.io/nvidia/digits:$VER
