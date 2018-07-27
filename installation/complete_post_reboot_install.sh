381a063c32218609ba7b245ebeb3641c0bb600ac
Nox <fp9821@gmail.com>
2018-07-27 09:39:20 +0900

    add untested, combined scripts

---
+++ installation/complete_post_reboot_install.sh
@@ -0,0 +1,4 @@
+./3_install_cuda_post-reboot.sh
+./4_install_cudnn.sh
+./5_install_docker.sh
+./6_install_nvidia-docker.sh
---
+++ installation/driver_nvidia-docker.sh
@@ -0,0 +1,3 @@
+./1_install_nvidia-driver.sh
+./5_install_docker.sh
+./6_install_nvidia-docker.sh
